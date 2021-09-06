cbd739e7842795ca3926fb1a4e26f6d213ae7358
Silvia Salat <salat.silvia@gmail.com>
2021-09-06 09:06:40 +0200

    FIX(navbar) js closing when click on link

--- js/navbar.js
+++ js/navbar.js
@@ -6,3 +6,11 @@ const body = document.body;
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
 });
+
+var menu = document.querySelectorAll(".nav-link");
+
+menu.forEach((x, i) =>
+  x.addEventListener("click", () => {
+    navbar.classList.toggle("change");
+  })
+);
